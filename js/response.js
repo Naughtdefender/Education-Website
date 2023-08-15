09a37315dc6953e7b1974de9fb49fd7751d5b2ca
Kshitiz Sharma <kshitizsharma405@gmail.com>
2023-08-15 15:41:11 +0530

    last commit

--- js/response.js
+++ js/response.js
@@ -3,7 +3,7 @@ const navbar = document.querySelector(".navbar");
 const navList = document.querySelector(".nav-list");
 const rightNav = document.querySelector(".rightNav");
 
-burger.addEventListener('click', ()=>{
+burger.addEventListe    ner('click', ()=>{
     rightNav.classList.toggle('v-class');
     navList.classList.toggle('v-class');
     navbar.classList.toggle('h-nav');
